e448bce9ac510337f52ec3bc4afb5e103fc75caa
Ahmad Elghafari <ahmadelghafari@gmail.com>
2019-05-09 21:19:46 +0000

    Polished python code

--- system_daemon/setup.py
+++ system_daemon/setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup
 
 NAME='pyoiz'
-VERSION='0.1.1'
+VERSION='0.1.0'
 DESCRIB='Practicing to building FreeBSD Packages out of Python code'
 LON_DESC='Why to say a lot?'
 PROJ_URL='https://github.com/tonight-halfmoon/'
 setup(name=NAME,
-	version=VERSION,
-	description=DESCRIB,
-	long_description=LON_DESC,
-	url=PROJ_URL,
-	py_modules=['py'],
-	setup_requires=["setuptools-pkg"],
-	entry_points={
-		'console_scripts': [ 'start_poiz=py:py_start',
-				     'stop_poiz=py:py_stop' 
-				]	
-	}
-     )
+      version=VERSION,
+      description=DESCRIB,
+      long_description=LON_DESC,
+      url=PROJ_URL,
+      py_modules=['oiz'],
+      setup_requires=["setuptools-pkg"],
+      entry_points={
+        'console_scripts': [ 'start_poiz=oiz:oiz_start',
+                             'stop_poiz=py:oiz_stop'
+        ]
+      }
+)
